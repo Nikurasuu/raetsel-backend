236e494a57159f7e6c8c20c3d0157f065d7cbed5
Niklas Malkusch <niklasmalkusch@googlemail.com>
2024-09-04 13:08:02 +0200

    Added words database and first access functions

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/kamva/mgm/v3"
 	"github.com/nikurasuu/raetsel-backend/internal/config"
 	"github.com/nikurasuu/raetsel-backend/internal/server"
+	"github.com/nikurasuu/raetsel-backend/internal/wordlist"
 	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -14,12 +15,17 @@ func main() {
 	cfg, _ := config.NewConfig()
 	logger := logrus.New()
 
-	err := mgm.SetDefaultConfig(nil, cfg.Mongo.DataBase, options.Client().ApplyURI("mongodb://"+cfg.Mongo.Host+":"+strconv.Itoa(cfg.Mongo.Port)))
-	if err != nil {
-		logger.Fatalf("Error setting up mgm: %v", err)
+	wordlist, wordlistErr := wordlist.NewWordList("words.db")
+	if wordlistErr != nil {
+		logger.Fatalf("Error creating wordlist: %v", wordlistErr)
 	}
 
-	server := server.NewServer(cfg, logger)
+	mgmErr := mgm.SetDefaultConfig(nil, cfg.Mongo.DataBase, options.Client().ApplyURI("mongodb://"+cfg.Mongo.Host+":"+strconv.Itoa(cfg.Mongo.Port)))
+	if mgmErr != nil {
+		logger.Fatalf("Error setting up mgm: %v", mgmErr)
+	}
+
+	server := server.NewServer(cfg, logger, wordlist)
 	if err := server.Start(); err != nil {
 		logger.Fatalf("Error starting the server: %v", err)
 	}
